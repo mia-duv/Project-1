b301dadce2cc33227a802c602d2fa6b70b367dd8
mia-duv <56006479+mia-duv@users.noreply.github.com>
2019-12-17 22:48:04 -0500

    merging with javits code

--- javascript/app.js
+++ javascript/app.js
@@ -11,11 +11,13 @@ var settings = {
 //Global variables
 var searchBarInput;
 var teams = [];
+var selectedTeam;
 var selectedTeamId;
 var selectedTeamStanding = {};
 var standings = [];
 var games = [];
-var selectedGame = [];
+var selectedGame;
+
 
 //Getting Team information 
 $.ajax(settings).done(function (response) {
@@ -31,7 +33,7 @@ $.ajax(settings).done(function (response) {
 
     // Created filter to search teams by nick name and team id. 
     searchBarInput = $("#searchBar").val().trim();
-    var selectedTeam = teams.filter(
+    selectedTeam = teams.filter(
       team => {
         return team.nickname.toLowerCase() == searchBarInput.toLowerCase()
       })[0];
@@ -49,57 +51,67 @@ $.ajax(settings).done(function (response) {
       // console.log("ST Standing", selectedTeamStanding)
 
       $.ajax(games).then(function (response) {
-    
-      for (var i = 0; i < response.api.games.length; i++) {
-        console.log(response.api.games[i].statusGame= "Scheduled");
-        
-      }      
+        // console.log(response.api.games.filter( team => team.hTeam.teamId === selectedTeamId));
+        games = response.api.games
+        selectedGame = games.filter(
+          game => {
+            return game.hTeam.teamId === selectedTeamId
+            
+          });
+          
+          console.log(selectedGame);
+     
 
-      // console.log("ST Standing WINS", selectedTeamStanding.win)
-      var selection = $("<div>")
-      var selectedTeamName = $("<h1>").text(selectedTeam.nickname)
-      var selectedTeamLogo = $("<img>").attr({
-        src: selectedTeam.logo,
-        width: "380px"
-      })
-      var selectedTeamRecord = $("<h2>").text("Wins: " + selectedTeamStanding.win + " Loss: " + selectedTeamStanding.loss);
-      selection.append(selectedTeamName, selectedTeamLogo, selectedTeamRecord);
-      // console.log(selection);
-      $(".table1").append(selection);
-    
+          // console.log("ST Standing WINS", selectedTeamStanding.win)
+          var selection = $("<div>")
+          var selectedTeamName = $("<h1>").text(selectedTeam.nickname)
+          var selectedTeamLogo = $("<img>").attr({
+            src: selectedTeam.logo,
+            width: "380px"
+          })
+          var selectedTeamRecord = $("<h2>").text("Wins: " + selectedTeamStanding.win + " Loss: " + selectedTeamStanding.loss);
+          selection.append(selectedTeamName, selectedTeamLogo, selectedTeamRecord);
+          // console.log(selection);
+          $(".table1").append(selection);
+          
+          var col2 = $("<div>");
+          var selectedTeamGame = $("<ol>").text(selectedGame);
+          col2.append(selectedTeamGame);
+          $("#UpGames").append(col2);
 
-      // Game info for column 2 
+
+
+
+        });
+      });
 
     });
+
+    // Get team records 
+    var record = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://api-nba-v1.p.rapidapi.com/standings/standard/2019",
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
+        "x-rapidapi-key": "743c363af9msh8937f184c8dcec0p1bf377jsn68ba5af98b0c"
+      }
+    }
+
+    //Column 2 - Game Information 
+
+    var games = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://api-nba-v1.p.rapidapi.com/games/seasonYear/2019",
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
+        "x-rapidapi-key": "32f3549c71mshfad8e66e3e3891dp11a5b4jsn37e2605ac94b"
+      }
+    
+    }
+  
+
   });
-
-})
-
-// Get team records 
-var record = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://api-nba-v1.p.rapidapi.com/standings/standard/2019",
-  "method": "GET",
-  "headers": {
-    "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
-    "x-rapidapi-key": "743c363af9msh8937f184c8dcec0p1bf377jsn68ba5af98b0c"
-  }
-}
-
-//Column 2 - Game Information 
-
-var games = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://api-nba-v1.p.rapidapi.com/games/seasonYear/2019",
-  "method": "GET",
-  "headers": {
-    "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
-    "x-rapidapi-key": "32f3549c71mshfad8e66e3e3891dp11a5b4jsn37e2605ac94b"
-  }
-}
-
-
-
-});
